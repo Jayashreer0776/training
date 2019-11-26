6f7b9139d7b4e7436a330db33cf6217d96b0cfcb
Jayashree <Jayashree.r@sonata-software.com>
2019-11-26 17:51:31 +0530

    This is my first java git commit.

---
+++ Training/src/P1/Calculation.java
@@ -0,0 +1,21 @@
+package P1;
+
+public class Calculation {
+
+	int Num1;
+	int Num2;
+	
+	void Add(){
+		System.out.print(Num1+Num2);
+	}
+	void Subtract(){
+		System.out.print(Num1-Num2);
+	}
+	void Multiply(){
+		System.out.print(Num1*Num2);
+	}
+	void Divide(){
+		System.out.print(Num1/Num2);
+	}
+	
+}
---
+++ Training/src/P1/Driver.java
@@ -0,0 +1,15 @@
+package P1;
+
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		Calculation C1 = new Calculation(); 
+		C1.Num1=1;
+		C1.Num2=2;
+		C1.Add();
+
+	}
+
+}
